a8f1e6220910b9e92a541067ecac468b15133eb9
Alan Medina 📚 <Alan-x-x@hotmail.com>
2025-05-12 22:42:00 -0600

    Corrección de dependencias.
    Se tenia un error de versiones las cuales las dependencias de tailwind se modificaron manualmente.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Agrega las rutas de tus archivos fuente
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
